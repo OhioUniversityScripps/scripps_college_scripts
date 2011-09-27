9f4220a55a0bb6ae2f19b74e6f7653e93028572e
Ricky Chilcott <ricky@rbsavvy.com>
2011-09-27 13:14:42 -0400

    OIT's Footprints Statusbar Project

---
+++ Footprints Statusbar/Footprints Statusbar/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Footprints Statusbar
+#
+#  Created by Ricky Chilcott on 9/19/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
