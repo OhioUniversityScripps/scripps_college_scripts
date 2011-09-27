3dca2b73772717502c43b28841a5e3e45e4508c2
Ricky Chilcott <ricky@rbsavvy.com>
2011-09-27 13:14:04 -0400

    Removing MunkiServer scripts from repository

--- munkiserver_postflight/http.rb
+++
@@ -1,9 +0,0 @@
-require "net/http"
-require "uri"
-
-uri = URI.parse("http://www.google.com/")
-
-# Full
-http = Net::HTTP.new(uri.host, uri.port)
-response = http.request(Net::HTTP::Get.new(uri.request_uri))
-puts response
