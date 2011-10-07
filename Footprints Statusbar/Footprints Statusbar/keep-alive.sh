1552774ec39612aee56c6436ae0ed2597b4a471f
Ricky Chilcott <ricky@rbsavvy.com>
2011-10-07 17:44:53 -0400

    Building Footprints Statusbar installer package with launchd tasks

---
+++ Footprints Statusbar/Footprints Statusbar/keep-alive.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  keep-alive.sh
+#  Footprints Statusbar
+#
+#  Created by Ricky Chilcott on 10/6/11.
+#  Copyright 2011 __Ohio University__. All rights reserved.
+open /Applications/Footprints\ Statusbar.app
