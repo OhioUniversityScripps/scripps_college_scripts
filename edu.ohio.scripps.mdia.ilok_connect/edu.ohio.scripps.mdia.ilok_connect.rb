1ef4f7f4ba5458116046de53ade4442cb64b8116
Logan Zimmerer <lz298708@ohio.edu>
2011-12-01 09:57:49 -0500

    Using absolute paths, we have added the directory where the pkg will be installed to for the plists and csv file.

--- edu.ohio.scripps.mdia.ilok_connect/edu.ohio.scripps.mdia.ilok_connect.rb
+++ edu.ohio.scripps.mdia.ilok_connect/edu.ohio.scripps.mdia.ilok_connect.rb
@@ -8,14 +8,14 @@ test = false
 
 #Copy Default Plists
 `mkdir /Users/$USER/Library/Preferences/silex/SXVirtualLink`
-`cp jp.silex.configuration.plist /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.configuration.plist`
-`cp default-jp.silex.favorites.plist /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.favorites.plist`
+`cp /Library/Management/ilok_connect/jp.silex.configuration.plist /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.configuration.plist`
+`cp /Library/Management/ilok_connect/default-jp.silex.favorites.plist /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.favorites.plist`
 
 #Modify jp.silex.favorites.plist with device specific settings
 mac_address = `ifconfig en0 | awk '/ether/ {print $2}'`.chomp
 puts "My MAC address is #{mac_address}" if test
 
-CSV.open('data.csv', 'r') do |row|
+CSV.open('/Library/Management/ilok_connect/data.csv', 'r') do |row|
   if row[0].downcase == mac_address.downcase
     connect_automatically_enable = "/usr/libexec/plistbuddy -c \"Set :#{row[1]}:Connect\\ Automatically\\ Enable true\" /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.favorites.plist"
     connect_automatically = "/usr/libexec/plistbuddy -c \"Set :#{row[1]}:Connect\\ Automatically 1\" /Users/$USER/Library/Preferences/silex/SXVirtualLink/jp.silex.favorites.plist"
