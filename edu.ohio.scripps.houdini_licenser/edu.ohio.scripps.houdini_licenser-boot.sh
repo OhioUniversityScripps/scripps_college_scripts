c6f2eb2729bbf3721a213f51eeb90c96b6949b6e
Ricky Chilcott <ricky@rbsavvy.com>
2011-12-28 08:13:26 -0500

    Modification of Houdini Licenser which uses find to make these scripts more portable and more accurate

--- edu.ohio.scripps.houdini_licenser/edu.ohio.scripps.houdini_licenser-boot.sh
+++ edu.ohio.scripps.houdini_licenser/edu.ohio.scripps.houdini_licenser-boot.sh
@@ -3,19 +3,12 @@
 # Remove machine licenses if not in the same month
 # On next launch, user will be prompted to install new licenses
 LICENSE_FOLDER_LOC="/Library/Preferences/sesi"
-LICENSE_FILE_LOC="$LICENSE_FOLDER_LOC/licenses"
 
 if [ -e $LICENSE_FILE_LOC ]; then
 	echo "Houdini license files exist"
 	
-	currentMonth=$(date | awk '{print $2}')
-	fileMonth=$(ls -la $LICENSE_FILE_LOC | awk '{print $6}')
-
-	# If it's not the same month, remove licenses, envoking a re-license!
-	if [ $currentMonth != $fileMonth ]; then
-		echo "Houdini licenses are invalid. Removing."
-		rm -rf $LICENSE_FOLDER_LOC
-	else
-		echo "Houdini licenses still valid"
-	fi
+	# Delete all license files if they are older than 28 days
+	# Licenses expire after 30 days, but 28 cycles will take care of
+	# any issues related with not rebooting at the currect time(s)
+	find $LICENSE_FOLDER_LOC -name license* -and -ctime -25d -exec rm {} \;
 fi
