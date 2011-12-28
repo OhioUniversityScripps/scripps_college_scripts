c0b969cc9e7008bd943961391d2f7e965c57d285
Ricky Chilcott <ricky@rbsavvy.com>
2011-12-28 08:15:04 -0500

    Modifying the script to cycle ever 28 days instead of 25 days

--- edu.ohio.scripps.houdini_licenser/edu.ohio.scripps.houdini_licenser-boot.sh
+++ edu.ohio.scripps.houdini_licenser/edu.ohio.scripps.houdini_licenser-boot.sh
@@ -10,5 +10,5 @@ if [ -e $LICENSE_FILE_LOC ]; then
 	# Delete all license files if they are older than 28 days
 	# Licenses expire after 30 days, but 28 cycles will take care of
 	# any issues related with not rebooting at the currect time(s)
-	find $LICENSE_FOLDER_LOC -name license* -and -ctime -25d -exec rm {} \;
+	find $LICENSE_FOLDER_LOC -name license* -and -ctime -28d -exec rm {} \;
 fi
