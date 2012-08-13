67d861ce7f9522e02b006f29d13acebadce0bdfd
Ricky Chilcott <ricky@rbsavvy.com>
2012-08-13 13:37:09 -0400

    Removing older scrips that are no longer used.

--- edu.ohio.scripps.lion_resume_disable/lion_resume_disable.sh
+++
@@ -1,7 +0,0 @@
-#Lock the Saved Application State Folder
-chflags uchg "$HOME/Library/Saved Application State"
-
-#Lock the BYHOST
-BYHOST=$(system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $3}')
-chflags uchg "$HOME/Library/Preferences/ByHost/com.apple.loginwindow.$BYHOST.plist"
-
