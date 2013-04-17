fcb453833c412a36c5be5d63efc436096befaa8c
Ricky Chilcott <ricky@rickychilcott.com>
2013-04-17 08:04:01 -0400

    Install Fetch the Proper way! Only the app, and license in launchd task each login.

---
+++ com.fetchsoftworks.fetch/fetch_licenser.sh
@@ -0,0 +1,4 @@
+DOMAIN="${HOME}/Library/Preferences/com.fetchsoftworks.Fetch"
+
+defaults write ${DOMAIN} "RegistrantName" -string "Ohio University"
+defaults write ${DOMAIN} "SerialNumber" -string "FETCHEX001-NZGP-KV3C-PS9B-UAMU-SWDW"
