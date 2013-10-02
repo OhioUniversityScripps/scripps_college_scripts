cdcfc201ce94023eda28a4367f0d29fdf95e0af7
Ricky Chilcott <ricky@rickychilcott.com>
2013-10-02 11:01:40 -0400

    Remove outdated users

--- edu.ohio.remove_outdated_users/edu.ohio.remove_outdated_users.sh
+++ edu.ohio.remove_outdated_users/edu.ohio.remove_outdated_users.sh
@@ -5,7 +5,6 @@ KEEP=$( cat <<EOL
 /Users/admin
 /Users/administrator
 /Users/sccadmin
-/Users/mdiaadmin
 /Users/Shared
 /Users/Search
 EOL);
@@ -16,14 +15,20 @@ DAYS=30
 USERLIST=`find /Users -type d -maxdepth 1 -mindepth 1 -not -name "*.*" -mtime +$DAYS`
 
 for user in $USERLIST ; do
+  echo "Inspecting $user to determine whether it should be kept"
+  
   # Keep select accounts
-  for keep in $KEEP ; do
-    [[ "$user" == "$keep" ]] && user="" && continue
+  for kept_user in $KEEP ; do
+    if [[ "$user" == "$kept_user" ]]; then
+		echo "Will not delete $user"
+		user=""
+		break # no need to continue if we've found a match
+	fi
   done
   
   if [[ "$user" != "" ]]; then
     echo "Deleting account and home directory for" $user
     # dscl . delete $user  #delete the account  
-    # rm -r $user  #delete the home directory
+    rm -r $user  #delete the home directory
   fi
 done
