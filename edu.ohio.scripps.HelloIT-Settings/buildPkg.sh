540c22d4f9521d708d89321a654f7448c3ccece3
Ricky Chilcott <ricky@rbsavvy.com>
2016-05-06 10:15:39 -0400

    Update build script

--- edu.ohio.scripps.HelloIT-Settings/buildPkg.sh
+++ edu.ohio.scripps.HelloIT-Settings/buildPkg.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-../munki-pkg/munkipkg --sync .
-../munki-pkg/munkipkg --export-bom-info .
+# Cleanup
+rm -rf build/HelloIT-Settings.pkg
+rm -rf build/HelloIT-Settings.dmg
+
+# Build Package and wrap in DMG
+munkipkg .
+dropdmg build/HelloIT-Settings.pkg
+
+open build/
